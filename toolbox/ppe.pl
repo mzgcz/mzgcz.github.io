f7d27aa35bb4a0be90a6d17eba6c00d76244dc83
mzgcz <j.wenjiao@gmail.com>
2014-05-05 17:14:47 +0800

    使用新的pod2html引擎

--- toolbox/ppe.pl
+++ toolbox/ppe.pl
@@ -1,117 +1,151 @@
 #!/usr/bin/env perl
 
-# article, title, sections, heading, caption, paragraphs, list
+# 文档（数据）结构模型
+# +---------+
+# | article |
+# +---------+
+#      |
+#      v
+#  +-------+    +---------+    +---------+
+#  | title |--->| section |--->| section |--->...
+#  +-------+    +---------+    +---------+
+#                    |
+#                    v
+#          +---------+---------+    +-----------+    +------+
+#          | heading | caption |--->| paragraph |--->| list |--->...
+#          +---------+---------+    +-----------+    +------+
+#                                                        |
+#                                                        v
+#                                                     +------+    +------+
+#                                                     | item |--->| item |--->...
+#                                                     +------+    +------+
+#                                                        |
+#                                                        v
+#                                                     +-------+    +------+    +-----------+
+#                                                     | entry |--->| list |--->| paragraph |--->...
+#                                                     +-------+    +------+    +-----------+
 
 use strict;
 use warnings;
 use Template;
 use Pod::Parser;
 
+# 解析POD文档，生成相应的html格式
 package PPE;
 our @ISA = qw(Pod::Parser);
 
 my %article;
+my @article;
+my @list_stack;
 
+# 将command分为两类：1. 独立的， 2. 成对的
 sub article_grow {
   my ($type, $content) = @_;
-
   chomp $content;
   chomp $content;
 
-  if ($type eq "title") {
-    $article{"title"} = $content;
-  } elsif ($type eq "heading") {
-    my $sections = $article{"sections"};
-    my $new_no = 0;
-    if (defined $sections) {
-      $new_no = @$sections;
-    }
-    $article{"sections"}->[$new_no]->{"heading"} = $content;
+  if ($type eq 'head1') {
+    $article[0] = {title => $content};
+  } elsif ($type eq 'head2') {
+    my $no = @article;
+    my @section;
     if ($content =~ /">(.*?)<\/a>/) {
-      $article{"sections"}->[$new_no]->{"caption"} = $1;
+      $section[0] = {heading => $content,
+                     caption => $1};
     } else {
-      $article{"sections"}->[$new_no]->{"caption"} = $content;
+      $section[0] = {heading => $content,
+                     caption => $content};
     }
-  } elsif ($type eq "paragraph") {
-    my $sections = $article{"sections"};
-    my $last_no = @$sections-1;
-    my $paragraphs = $sections->[$last_no]->{"paragraphs"};
-    if (defined $paragraphs) {
-      push @$paragraphs, $content;
-    } else {
-      $article{"sections"}->[$last_no]->{"paragraphs"}->[0] = $content;
+    $article[$no] = {section => \@section};
+  } elsif ($type eq 'paragraph') {
+    my $item;
+    if (@list_stack <= 0) {            # 处于非列表环境
+      $item = $article[-1]->{section}; # 最后的section
+    } else {                           # 处于列表环境
+      $item = $list_stack[-1]->[-1]->{item}; # 最后的item
     }
-  } elsif ($type eq "entry") {
-    my $sections = $article{"sections"};
-    my $last_no = @$sections-1;
-    my $list = $sections->[$last_no]->{"entrys"};
-    if (defined $list) {
-      push @$list, $content;
-    } else {
-      $article{"sections"}->[$last_no]->{"entrys"}->[0] = $content;
+    my $no = @$item;
+    $item->[$no] = {paragraph => $content};
+  } elsif ($type eq 'over') {
+    my $item;
+    if (@list_stack <= 0) {            # 处于非列表环境
+      $item = $article[-1]->{section}; # 最后的section
+    } else {                           # 处于列表环境
+      $item = $list_stack[-1]->[-1]->{item}; # 最后的item
     }
+    my @list;
+    my $no = @$item;
+    $item->[$no] = {list => \@list};
+    push @list_stack, \@list;
+  } elsif ($type eq 'back') {
+    pop @list_stack;
+  } elsif ($type eq 'item') {
+    my @item;
+    my $list = $list_stack[-1]; # push, pop操作的是列表最后一个元素
+    my $no = @$list;
+    $item[0] = {entry => $content};
+    $list->[$no] = {item => \@item};
+  } else {
+    print "unknown type: $type => $content!\n";
   }
 }
 
 sub command {
-    my ($parser, $command, $paragraph, $line_num) = @_;
-    # print $paragraph."--".$command."--> 1\n";
-    my $expansion = $parser->interpolate($paragraph, $line_num);
+  my ($parser, $command, $paragraph, $line_num) = @_;
 
-    ## Interpret the command and its text; sample actions might be:
-    if ($command eq 'head1') {
-      article_grow("title", $expansion);
-    } elsif ($command eq 'head2') {
-      article_grow("heading", $expansion);
-    } elsif ($command eq 'item') {
-      article_grow("entry", $expansion);
-    }
-    ## ... other commands and their actions
+  my $expansion = $parser->interpolate($paragraph, $line_num);
+  article_grow($command, $expansion);
 }
 
-sub verbatim {
-    my ($parser, $paragraph, $line_num) = @_;
-    # print $paragraph."--> 2\n";
-    ## Format verbatim paragraph; sample actions might be:
-    my $out_fh = $parser->output_handle();
-    # print $out_fh $paragraph;
-}
-
-sub textblock {
-    my ($parser, $paragraph, $line_num) = @_;
-    # print $paragraph."--> 3\n";
-    ## Translate/Format this block of text; sample actions might be:
-
-    my $expansion = $parser->interpolate($paragraph, $line_num);
-
-    article_grow("paragraph", $expansion);
-}
-
+# 支持 L<A|B>表示A为链接名，B为链接地址
 sub interior_sequence {
-    my ($parser, $seq_command, $seq_argument) = @_;
-    ## Expand an interior sequence; sample actions might be:
-    return "*$seq_argument*"     if ($seq_command eq 'B');
-    return "`$seq_argument'"     if ($seq_command eq 'C');
-    return "_${seq_argument}_'"  if ($seq_command eq 'I');
-    if ($seq_command eq 'L') {
-      if ($seq_argument =~ /(.+?)\|(.+)/) {
-        return qq(<a href="$2">$1</a>);
-      }
+  my ($parser, $seq_command, $seq_argument) = @_;
+  if ($seq_command eq 'L') {
+    if ($seq_argument =~ /(.+?)\|(.+)/) {
+      return qq(<a href="$2">$1</a>);
     }
-    ## ... other sequence commands and their resulting text
+  }
+}
+
+# 支持多段普通文本和html文本
+sub textblock {
+  my ($parser, $paragraph, $line_num) = @_;
+
+  my $expansion = $parser->interpolate($paragraph, $line_num);
+  article_grow('paragraph', $expansion);
 }
 
 package main;
 
-my ($pod_file, $tt_file, $html_file) = @ARGV;
+sub get_html_name {
+    my $pod_file = shift;
+
+    if ($pod_file =~ /(.*)\.pod/) {
+        $1.".html";
+    } else {
+        die "Not A Pod File Error!\n";
+    }
+}
+
+my $pod_file = $ARGV[0] || die "No Pod File Error!\n";
+my $tt_file = $ARGV[1] || "common.tt";
+my $html_file = $ARGV[2] || get_html_name($pod_file);
 
 my $parser = new PPE();
 $parser->parse_from_file($pod_file);
 
-# 生成目标网页
+# 生成网页
+my $home_dir = "/home/mzgcz/game_room/mzgcz.github.io/";
+my $tt_path = $home_dir."template/";
 my $tt = Template->new({
-                        INCLUDE_PATH => "/home/mzgcz/game_room/mzgcz.github.io/template/",
+                        INCLUDE_PATH => $tt_path,
                         INTERPOLATE  => 1,
                        }) || die "$Template::ERROR\n";
 
+$article{article} = \@article;
 $tt->process($tt_file, \%article, $html_file) || die $tt->error();
+
+
+# 数据结构测试
+# use Data::Dumper;
+# print Dumper \@article;
